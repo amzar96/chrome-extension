0bf6a09c55ec2adf4efec5399fe2b24b2f887a65
amzar <mohdamzar96@gmail.com>
2023-12-17 04:57:59 +0800

    fix: get response headers

--- key-api-caller/background.js
+++ key-api-caller/background.js
@@ -13,10 +13,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             }
         })
             .then(response => {
-                console.log(response.status)
+                console.log(response.headers.get('x-api-request-id'));
+                // for (let entry of response.headers.entries()) {
+                //     console.log(entry);
+                // }
             })
             .catch(error => {
-                console.error(response)
+                console.error("error")
             });
     }
 });
