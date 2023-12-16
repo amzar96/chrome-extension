6432e8e127dc7efba36b034cc8de933bd428b4e0
amzar <mohdamzar96@gmail.com>
2023-12-17 05:57:24 +0800

    update: basic css and message

--- key-api-caller/background.js
+++ key-api-caller/background.js
@@ -3,8 +3,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === 'submitForm') {
         const email = request.email;
 
-        console.log(email)
-
         fetch('http://127.0.0.1:8000/health', {
             method: 'GET',
             // body: JSON.stringify({ id: email, password: 'password' }),
